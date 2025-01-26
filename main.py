5a19c8f0c48e6377bdfbf7dd588b66204c905628
foxhansik <pisynvmuke@gmail.com>
2025-01-26 13:24:04 +0300

    modified(hi): modified file hi and main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 print("hello")
 import telebot
-
+from hi import hi
 
 bot = telebot.TeleBot("7307712996:AAHo1rZNud-Re5hKc1j98u0xRp6FBNtb0zk")
 
+@bot.message_handler(commands=["hi", "hello"])
+def hello(message):
+    hi(message, bot)
 
 
 
