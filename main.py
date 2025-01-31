e1be09f258a4753a551d959385ef6e614f490dc8
DIMITRY <tygfhv465@gmail.com>
2025-01-31 10:01:45 +0300

    feature(start): added a start func

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 print("hello")
 import telebot
-
+from start import start_
 
 bot = telebot.TeleBot("7307712996:AAHo1rZNud-Re5hKc1j98u0xRp6FBNtb0zk")
 
-
+def start_go(message):
+    start_(message)
 
 
 bot.infinity_polling()
--- start.py
+++ start.py
@@ -1,3 +1,13 @@
-
-def help(mes):
-    bot.send_message(mes.chat.id, "молодец")
+import telebot
+import config
+bot = telebot.TeleBot(config.tok)
+def start_(mes):
+    bot.send_message(mes.chat.id, "Приветствую!")
+    #заменить "..." на команды#
+    bot.send_message(mes.chat.id, "команды:\n"
+                                  "*/...\n"
+                                  "*/...\n"
+                                  "*/...\n"
+                                  "*/...\n"
+                                  "*/...\n"
+                                  "*/...")
