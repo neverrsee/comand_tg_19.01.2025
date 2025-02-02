c9be44b2bbfb882493d854945acbd220bd075d8c
Never <Megalovania4gg@gmail.com>
2025-02-02 13:25:46 +0300

    feature main_nenonen merge buttons

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 print("hello")
 import telebot
-
+from telebot import types, bot, message
+from buttons import links
 
 bot = telebot.TeleBot("7307712996:AAHo1rZNud-Re5hKc1j98u0xRp6FBNtb0zk")
 
+@bot.message_handler(commands='links')
+def links(message):
+    links(message, bot, types)
 
 
 bot.infinity_polling()
