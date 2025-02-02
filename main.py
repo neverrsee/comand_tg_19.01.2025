1e9b2814d67a86f01ae922dc171412e8a5e0fcda
Rust <nikita_gorlachev@icloud.com>
2025-02-02 17:25:48 +0700

    new fich

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 print("hello")
 import telebot
-
+import buttons
+import Nikita
+from telebot import types
 
 bot = telebot.TeleBot("7307712996:AAHo1rZNud-Re5hKc1j98u0xRp6FBNtb0zk")
 
-
+@bot.message_handler(commands=['start'])
+def buttons_start(message):
+    Nikita.start(message,bot,types)
 
 bot.infinity_polling()
 
