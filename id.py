125c2c772ea03f113be36da32271aaafc94f5a96
Feunlic <Anna.Eremenko2009@rambler.ru>
2025-02-02 13:54:51 +0300

    id.py, main.py

--- id.py
+++ id.py
@@ -1,7 +1,3 @@
-
-
-text = ""
-
 def user_id(message, bot):
     # создать переменную для хранения айди пользователя
     user_id = message.chat.id
@@ -10,3 +6,10 @@ def user_id(message, bot):
     if "id" in text:
         bot.sed_message(message.chat.id, f"You ID: {message.from_user.id}")
     # бот отправит сообщение человеку с айди пользователя
+
+def answer(call, types, bot):
+    if call.data == "yes":
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_id = types.KeyboardButton("ID")
+        markup_reply.add(btn_id)
+        bot.send_message(call.message.chat.id, "Выбериб что тебе интересует")
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 print("hello")
 import telebot
-
+import id
 
 bot = telebot.TeleBot("7307712996:AAHo1rZNud-Re5hKc1j98u0xRp6FBNtb0zk")
 
+player = []
+
+@bot.message_handler(commands=["id"])
+def info(message):
+    id.user_id(message, bot)
 
 
 bot.infinity_polling()
